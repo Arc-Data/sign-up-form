b5cafa0dd0d9598c4a338fd9e349ea383860e657
Arc-Data <arclight04@gmail.com>
2022-12-05 14:56:12 +0800

    starting code on form validation

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Working!")
+const submitBtn = document.querySelector('.btn');
+
+const patterns = {
+	'fname':/^\w+( +\w+)*$/g,
+	'lname':/^\w+( +\w+)*$/g,
+	'email':/^[\w0-9]+@[a-zA-Z]\.[a-zA-Z]+/g,
+};
+
+const pass1 = document.querySelector('#password1');
+const pass2 = document.querySelector('#password2');
+
+const inputs 
+
+function validateForm() {
+
+}
+
+submitBtn.addEventListener('click', validateForm)
